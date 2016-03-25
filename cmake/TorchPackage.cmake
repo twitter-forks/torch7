1336fba060d8d1c46d6b9337ea6f5e30e6656b09
Luke Alonso <lalonso@twitter.com>
2016-03-24 18:42:33 -0700

    static build patches

--- cmake/TorchPackage.cmake
+++ cmake/TorchPackage.cmake
@@ -1,24 +1,36 @@
 # -*- cmake -*-
 
+MACRO(ADD_TORCH_LIBRARY package type src)
+IF ("${type}" STREQUAL "STATIC")
+  if ("${src}" MATCHES "cu$" OR "${src}" MATCHES "cu;")
+    CUDA_ADD_LIBRARY(${package} STATIC ${src})
+  else()
+    ADD_LIBRARY(${package} STATIC ${src})
+  endif()
+ELSE()
+  IF ("$ENV{TH_ONLY_STATIC}" STREQUAL "YES")
+    ADD_LIBRARY(${package} ${type} "")
+    SET_TARGET_PROPERTIES(${package} PROPERTIES LINKER_LANGUAGE C)
+  ELSE()
+    if ("${src}" MATCHES "cu$" OR "${src}" MATCHES "cu;")
+      CUDA_ADD_LIBRARY(${package} ${type} ${src})
+    else()
+      ADD_LIBRARY(${package} ${type} ${src})
+    endif()
+  ENDIF()
+ENDIF()
+ENDMACRO()
+
 MACRO(ADD_TORCH_PACKAGE package src luasrc)
+
   INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR})
   INCLUDE_DIRECTORIES(${Torch_LUA_INCLUDE_DIR})
 
- ### C/C++ sources
- # As per CMake doc, macro arguments are not variables, so simple test syntax not working
+  ### C/C++ sources
   IF(NOT "${src}" STREQUAL "")
 
-    if ("${src}" MATCHES "cu$" OR "${src}" MATCHES "cu;")
-      CUDA_ADD_LIBRARY(${package} MODULE ${src})
-      if(BUILD_STATIC)
-        CUDA_ADD_LIBRARY(${package}_static STATIC ${src})
-      endif()
-    else()
-      ADD_LIBRARY(${package} MODULE ${src})
-      if(BUILD_STATIC)
-        ADD_LIBRARY(${package}_static STATIC ${src})
-      endif()
-    endif()
+    ADD_TORCH_LIBRARY(${package} MODULE "${src}")
+    ADD_TORCH_LIBRARY(${package}_static STATIC "${src}")
 
     ### Torch packages supposes libraries prefix is "lib"
     SET_TARGET_PROPERTIES(${package} PROPERTIES
@@ -32,10 +44,10 @@ MACRO(ADD_TORCH_PACKAGE package src luasrc)
     ENDIF()
 
     if(BUILD_STATIC)
-      SET_TARGET_PROPERTIES(${package}_static PROPERTIES
-        COMPILE_FLAGS "-fPIC")
-      SET_TARGET_PROPERTIES(${package}_static PROPERTIES
-        PREFIX "lib" IMPORT_PREFIX "lib" OUTPUT_NAME "${package}")
+    SET_TARGET_PROPERTIES(${package}_static PROPERTIES
+      COMPILE_FLAGS "-fPIC")
+    SET_TARGET_PROPERTIES(${package}_static PROPERTIES
+      PREFIX "lib" IMPORT_PREFIX "lib" OUTPUT_NAME "${package}")
     endif()
 
     INSTALL(TARGETS ${package}
